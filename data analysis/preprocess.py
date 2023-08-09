3f117acd961703a63b082d948f26ee3aa8045dc5
cstefano <carlos.stefanofilho@gmail.com>
2023-08-09 20:31:29 -0300

    Added functions

--- data analysis/preprocess.py
+++ data analysis/preprocess.py
@@ -1,6 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn import metrics
 
 def rename_columns(df):
     renaming_dict = {'CO_MUNICIPIO_ESC': 'City code',
@@ -50,3 +55,48 @@ def plot_scores_distributions(df, tests, naming_dict, plot_specifics):
         
     plt.suptitle(plot_specifics['title'])
     plt.tight_layout()
+
+def train_ada_boost_model(df, test_size, random_state, features, target):
+    X_train, X_test, y_train, y_test = train_test_split(df[features], target, test_size = test_size, random_state = random_state)
+
+    ABRegressor = AdaBoostRegressor()
+    ABRegressor.fit(X_train, y_train)
+
+    return ABRegressor, X_train, X_test, y_train, y_test
+
+def evaluate_regressor(regressor, X_train, X_test, y_train, y_test):
+    regression_metrics = {}
+    
+    y_train_pred = regressor.predict(X_train)
+    y_test_pred = regressor.predict(X_test)
+
+    regression_metrics['r2_train'] = metrics.r2_score(y_train, y_train_pred)
+    regression_metrics['r2_test'] = metrics.r2_score(y_test, y_test_pred)
+    regression_metrics['MAE_train'] = metrics.mean_absolute_error(y_train, y_train_pred)
+    regression_metrics['MAE_test'] = metrics.mean_absolute_error(y_test, y_test_pred)
+    regression_metrics['MSLE_train'] = metrics.mean_squared_log_error(y_train, y_train_pred)
+    regression_metrics['MSLE_test'] = metrics.mean_squared_log_error(y_test, y_test_pred)
+
+    return regression_metrics
+
+def train_with_correlation_feature_selection(df, target, absolute_correlations, n_iter, regressor_type, test_size, random_state):
+    total_features = len(absolute_correlations)
+    step = total_features/n_iter
+
+    df_result = pd.DataFrame()
+
+    for i in range(0, n_iter):
+        features = absolute_correlations.index[0 + i*step:(i + 1)*step]
+
+        if regressor_type == 'AdaBoost':
+            regressor, X_train, X_test, y_train, y_test = train_ada_boost_model(df, test_size, random_state, features, target)
+
+        regression_metrics = evaluate_regressor(regressor, X_train, X_test, y_train, y_test)
+        regression_metrics['nFeatures'] = len(features)
+
+        df_result = pd.concat([df_result, pd.DataFrame(regression_metrics)], axis = 1)
+    
+    return df_result
+
+
+
